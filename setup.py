a3b1d6cfd8e79432d16c5675ec6592a40bdace43
ykahalan <89739501+ykahalan@users.noreply.github.com>
2025-03-11 21:48:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="border_detection_minimal",
-    version="0.1",
+    name="adaptive-resampling",
+    version="0.1.0",
+    author="Yunis Kahalan",
+    author_email="ykahalan@gmail.com",
+    description="A package for adaptive resampling of datasets using border detection.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ykahalan/adaptive-resampling",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     install_requires=[
         "numpy",
+        "scikit-learn",
     ],
-    description="Minimal library for classifying border and core points based on distance threshold.",
-    author=":)",
 )
